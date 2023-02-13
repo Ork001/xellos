f1431237dc0d86f651b33842f7cc2f08bbffa391
Y.I.N <109145190+Ork001@users.noreply.github.com>
2023-02-13 13:42:08 +0530

    Update play.js

--- Commands/Media/play.js
+++ Commands/Media/play.js
@@ -8,7 +8,7 @@ module.exports = {
   desc: "To play a song from youtube",
   category: "Media",
   usage: `play <song name>`,
-  react: "🍁",
+  react: "🎶",
   start: async (Miku, m, { text, prefix, args }) => {
     if (!args[0])
       return Miku.sendMessage(
