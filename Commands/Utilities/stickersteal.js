32694466bd352e496a4d581091ee4057707d5555
Y.I.N <109145190+Ork001@users.noreply.github.com>
2023-02-13 13:34:49 +0530

    Update stickersteal.js

--- Commands/Utilities/stickersteal.js
+++ Commands/Utilities/stickersteal.js
@@ -2,11 +2,11 @@ const { Sticker, createSticker, StickerTypes } = require('wa-sticker-formatter')
 
 module.exports = {
     name: "steal",
-    alias: ["stickersteal"],
+    alias: ["take"],
     desc: "To steal a sticker",
     category: "Utilities",
     usage: "steal <reply to sticker>",
-    react: "🍁",
+    react: "💱",
     start: async (Miku, m, { text, prefix,quoted,pushName,mime,args }) => {
         if(!args.join(" ")){
             var packName = pushName;
