e8368e14ece64e12ce0394d9db6cefe8d98b976b
Y.I.N <109145190+Ork001@users.noreply.github.com>
2023-02-14 15:26:47 +0530

    Update script.js

--- Commands/Core/script.js
+++ Commands/Core/script.js
@@ -11,7 +11,7 @@ module.exports = {
         let picURL = fs.readFileSync('./Page/BG.jpg')
         let repoInfo = await axios.get('https://api.github.com/repos/FantoX001/Atlas-MD')
         let repo = repoInfo.data
-        let txt = `      🧣 *${botName}'s HOME* 🧣\n\n*⚠️ 𝚃𝙾𝙼𝙰𝙽:* https://chat.whatsapp.com/JTTwcmElglZEKmPNrg8VRA\n\n*⚠️ ⭑⭑⭑★ 𝐁𝐎𝐍𝐓𝐄𝐍 ★⭑⭑⭑:* https://chat.whatsapp.com/CQKbWP7c3GNIk4SfE7Xej7\n\n\n*©️ 𝚁𝚎𝚐𝚊𝚛𝚍𝚜 - 𝙷𝙾𝙿𝙴*`
+        let txt = `      🧣 *${botName}'s HOME* 🧣\n\n*⚠️ 𝚃𝙾𝙼𝙰𝙽:* https://chat.whatsapp.com/EmHKEG43PJTG7hTwwcdxif\n\n*⚠️ ⭑⭑⭑★ 𝐁𝐎𝐍𝐓𝐄𝐍 ★⭑⭑⭑:* https://chat.whatsapp.com/Ee7PXxxUR88CjepXyRnnq6\n\n\n*©️ 𝚁𝚎𝚐𝚊𝚛𝚍𝚜 - 𝙷𝙾𝙿𝙴*`
         await Miku.sendMessage(m.from,{image:picURL, caption:txt},{quoted:m});
     }
 }
